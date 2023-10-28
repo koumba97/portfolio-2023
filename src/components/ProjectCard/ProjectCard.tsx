c855bfe417ddef04ca61bd120f1532466f251bd3
Koumba <koumba.keita@vadesecure.com>
2023-10-28 19:46:03 +0200

    Responsive Home page

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -4,9 +4,11 @@ interface ProjectCardProp {
     children: any;
 }
 const ProjectCard = ({ children }: ProjectCardProp) => {
+    const bgImage = 'https://www.echollywell.co.uk/wp-content/uploads/blank-00cc00_040004000.png';
     return (
         <div className="project-card">
-            <p>{children}</p>
+            <p className="project-title">Project's title</p>
+            <div className="project-card-content" style={{ backgroundImage: `url(${bgImage})` }}></div>
         </div>
     );
 };
