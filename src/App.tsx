1f0f1c1de6b0e2ab2319db9c38637b322a5c8dcd
Koumba <koumba.keita@vadesecure.com>
2023-10-29 19:24:19 +0100

    Set up default lang

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Project from './pages/Project/Project';
 import AppTemplate from './templates/AppTemplate/AppTemplate';
+import { useEffect } from 'react';
 
 const App = () => {
+    useEffect(() => {
+        if (localStorage.getItem('koum-lang') === null) {
+            localStorage.setItem('koum-lang', 'en');
+        }
+    }, []);
+
     return (
         <Routes>
             <Route path="/" element={<AppTemplate />}>
