c07dd2e03728121c6e0c3c9cb8546d2bb75cc185
Koumba <koumba.keita@vadesecure.com>
2023-10-29 19:27:01 +0100

    Improve CopyLink feature

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import ProjectCarousel from '../../components/ProjectCarousel/ProjectCarousel';
 import ButtonLink from '../../components/ui/ButtonLink/ButtonLink';
 import NavButton from '../../components/ui/NavButton/NavButton';
@@ -13,8 +13,6 @@ const Project = () => {
     const [nextProject, setNextProject] = useState<string>();
     const { getProjectById } = useContext(ProjectContext);
     const { projectId } = useParams<{ projectId: string }>();
-
-    const location = useLocation();
 
     const LAST_PROJECT_ID = '9';
     const FIRST_PROJECT_ID = '1';
@@ -56,7 +54,7 @@ const Project = () => {
             <ProjectCarousel images={project.images} />
             <div className="project-details">
                 <section>
-                    <NavButton id="link-button" type="link" outlined link={location.pathname} />
+                    <NavButton id="link-button" type="link" outlined link={window.location.href} />
                     <h1 className="title">{project.title}</h1>
                     <p className="p-details">
                         Lorem ipsum dolor sit amet, consectetuer amet adipiscing elit. Aenean commodo ligula eget et
